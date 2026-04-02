e04e5a4e2f53625a70f91a7df28455f8c113e751
AnnaSarnaglia <Anna.sarnaglia@edu.ufes.br>
2026-04-02 15:03:01 -0300

    fiz 1_1

--- exercicios-gerais/01_revisao/rev_01/prog.c
+++ exercicios-gerais/01_revisao/rev_01/prog.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <math.h>
+
+/* Problema: Suponha que você está desenvolvendo um jogo 2D de tiro ao alvo
+e  você  deseja  verificar  se  um  tiro  acertou  o  alvo.  Assuma  que  o  tiro  e  o  alvo  são
+círculos e são conhecidas suas posições e raios.
+
+
+
+Definição dos formatos de entrada e saída:
+• Entrada: As coordenadas X e Y do alvo e seu raio R. Em seguida, as coordenadas e
+o raio do disparo.
+• Saída: Seu programa deverá informar se o tirou acertou o alvo.
+Ver exemplos de formato de entrada e saída nos arquivos fornecidos com a questão.
+ */
 
 int main()
 {
 
-    int a = 1;
-    printf("%d", a + a);
+    float Xa, Ya, Ra, Xd, Yd, Rd, d, raio2;
+
+    scanf("%f %f %f %f %f %f", &Xa, &Ya, &Ra, &Xd, &Yd, &Rd);
+    raio2 = Ra + Rd;
+    d = sqrt(pow(Xa - Xd, 2) + pow(Ya - Yd, 2));
+
+    if (raio2 >= d)
+    {
+        printf("ACERTOU");
+    }
+    else
+    {
+        printf("ERROU");
+    }
 
     return 0;
 }
